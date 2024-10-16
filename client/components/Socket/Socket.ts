70143f9e59c30890bd3b228b4f9f5329c616f213
Zachary Alfano <redninjadolphin1@hotmail.com>
2024-10-16 15:25:19 -0400

    socket cors fix

--- client/components/Socket/Socket.ts
+++ client/components/Socket/Socket.ts
@@ -3,4 +3,8 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  withCredentials: true, // Ensure credentials are sent with CORS
+  transports: ['websocket', 'polling'], // Use both websocket and polling transports
+});
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,29 @@ console.log(method([1, 2, 3]));
 
 let express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.set("port", 3001);
 
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin: any, callback: Function) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
 let http = require("http").Server(app);
-let io = require("socket.io")(http);
+let io = require("socket.io")(http, {
+  origin: 'http://localhost:3000', // Your React frontend URL
+  methods: ['GET', 'POST'],
+  credentials: true,
+});
 
 // simple '/' endpoint sending a Hello World
 // response
