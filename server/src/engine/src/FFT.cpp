3f1d3f3e2ea6e47550977f6863313be196a44205
Zachary Alfano <redninjadolphin1@hotmail.com>
2024-11-21 23:05:01 -0500

    gave up on space saving thingy

--- server/src/engine/src/FFT.cpp
+++ server/src/engine/src/FFT.cpp
@@ -6,47 +6,16 @@
 using std::vector;
 using std::complex;
 
-void rawFFTSpaceSaving(complex<double>* data, size_t n, size_t stride) {
-
-  // base case
-  if (n == 1) {
-    return;
-  }
-
-  const double NEGATIVE_TWO_PI_OVER_LEN = -2*M_PI/n;
-
-  rawFFTSpaceSaving(data, n/2, stride*2);
-  rawFFTSpaceSaving(data+stride, n/2, stride*2);
-
-  // minor optimization
-  complex<double> shift = exp(
-    complex<double>(0.0, NEGATIVE_TWO_PI_OVER_LEN) 
-  ); 
-  complex<double> cur = complex<double>(1.0, 0.0);
-
-  for (size_t i = 0; i < n/2; ++i) {
-    complex<double> even_value = data[i*2*stride];
-    complex<double> odd_value = data[i*2*stride + stride];
-
-    complex<double> odd_multiple = odd_value * cur;
-    cur *= shift;
-
-    data[i*stride] = even_value + odd_multiple;
-    data[(i+n/2)*stride] = even_value - odd_multiple;
-  }
-  
-}
-
-
 // uses pointers instead of vectors
-void rawFFT(complex<double>* data, size_t n) {
+void rawFFT(complex<double>* data, size_t n, bool inverse) {
 
   // base case
   if (n == 1) {
     return;
   } 
 
-  const double NEGATIVE_TWO_PI_OVER_LEN = -2*M_PI/n;
+  const double TWO_PI_OVER_LEN = 2*M_PI/n;
+  const double multiple = inverse ? TWO_PI_OVER_LEN : -TWO_PI_OVER_LEN;
 
   complex<double> odd[n/2];
   complex<double> even[n/2];
@@ -55,16 +24,16 @@ void rawFFT(complex<double>* data, size_t n) {
     even[i/2] = data[i];
   }
 
-  rawFFT(odd, n/2);
-  rawFFT(even, n/2);
+  rawFFT(odd, n/2, inverse);
+  rawFFT(even, n/2, inverse);
 
   // minor optimization
   complex<double> shift = exp(
-    complex<double>(0.0, NEGATIVE_TWO_PI_OVER_LEN) 
+    complex<double>(0.0, multiple) 
   ); 
   complex<double> cur = complex<double>(1.0, 0.0);
 
-  for (size_t i = 0; i < n/2; i++) {
+  for (size_t i = 0; i < n/2; ++i) {
     //complex<double> odd_multiple = odd[i] * exp(
       //complex<double>(0.0, i*NEGATIVE_TWO_PI_OVER_LEN) 
     //);
@@ -104,48 +73,13 @@ vector<complex<double>> FFTPadding(const vector<int16_t>& data) {
   }
   */
 
-  rawFFTSpaceSaving(raw_data, next_radix_2, 1);
+  rawFFT(raw_data, next_radix_2, false);
 
   for (size_t i = 0; i < next_radix_2; ++i) {
     output[i] = raw_data[i];
   }
 
   return output;
-}
-
-// uses pointers instead of vectors
-void rawIFFT(complex<double>* data, size_t n) {
-  const double TWO_PI_OVER_LEN = 2*M_PI/n;
-
-  // base case
-  if (n == 1) {
-    return;
-  } else {
-    complex<double> odd[n/2];
-    complex<double> even[n/2];
-    for (size_t i = 0; i < n; i+=2) {
-      odd[i/2] = data[i+1];
-      even[i/2] = data[i];
-    }
-
-    rawIFFT(odd, n/2);
-    rawIFFT(even, n/2);
-
-    // minor optimization
-    complex<double> shift = exp(
-      complex<double>(0.0, TWO_PI_OVER_LEN) 
-    ); 
-    complex<double> cur = complex<double>(1.0, 0.0);
-
-    for (size_t i = 0; i < n/2; i++) {
-      complex<double> odd_multiple = odd[i] * cur;
-      cur *= shift;
-
-      data[i] = even[i] + odd_multiple;
-      data[i+n/2] = even[i] - odd_multiple;
-    }
-
-  }
 }
 
 vector<int16_t> IFFTPadding(const vector<complex<double>>& data) {
@@ -164,7 +98,7 @@ vector<int16_t> IFFTPadding(const vector<complex<double>>& data) {
     raw_data[i] = data[i];
   }
 
-  rawIFFT(raw_data, next_radix_2);
+  rawFFT(raw_data, next_radix_2, true);
 
   for (size_t i = 0; i < len; ++i) {
     output[i] = (double)(raw_data[i].real())/len;
